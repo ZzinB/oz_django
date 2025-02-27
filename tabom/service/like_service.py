82b4362694360ae957f3aa1cab8f92d3038e5892
shinbilee <dltlsql8389@naver.com>
2025-02-27 15:11:07 +0900

    prevent duplicate likes

--- tabom/service/like_service.py
+++ tabom/service/like_service.py
@@ -5,3 +5,8 @@ def do_like(user_id: int, article_id: int) -> Like | None:
     if user_id and article_id:
         Like.objects.create(user_id=user_id, article_id=article_id)
     return None
+
+
+def undo_like(user_id: int, article_id: int) -> None:
+    like = Like.objects.filter(user_id=user_id, article_id=article_id).get()
+    like.delete()
