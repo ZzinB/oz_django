ff0b88fc97d892c96b884c56f668f7e538df21a4
shinbilee <dltlsql8389@naver.com>
2025-02-28 10:58:17 +0900

    test with coverage

--- tabom/service/like_service.py
+++ tabom/service/like_service.py
@@ -1,10 +1,8 @@
 from tabom.models import Like
 
 
-def do_like(user_id: int, article_id: int) -> Like | None:
-    if user_id and article_id:
-        Like.objects.create(user_id=user_id, article_id=article_id)
-    return None
+def do_like(user_id: int, article_id: int) -> Like:
+    return Like.objects.create(user_id=user_id, article_id=article_id)
 
 
 def undo_like(user_id: int, article_id: int) -> None:
